34d3f42c7a9f2d818be2888e23578238b38769d4
qqyvip013 <qqyvip013@163.com>
2020-11-04 21:10:19 +0800

    submit secret

---
+++ docs/.vuepress/secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+  appId: '8LKSqF37Lqc3pavKNd8mFLLm-gzGzoHsz',
+  appKey: 'lrdAG7gH3nwMDFK4fkQ3f2RV',
+  notify: false,
+  verify: false,
+  avatar: 'howe',
+  placeholder: '欢迎留言...'
+}
